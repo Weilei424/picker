9493e7355cdab5d2992aec6b2f78c816f3b6955f
Zhenxu Wang <mason123wp@gmail.com>
2023-07-04 15:27:53 -0400

    implement Item.java

--- src/main/java/com/mason/picker/entity/Item.java
+++ src/main/java/com/mason/picker/entity/Item.java
@@ -1,4 +1,26 @@
 package com.mason.picker.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "item")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "name")
+    @NotBlank(message = "Item name cannot be blank")
+    private String name;
+
+    @Column(name = "own_by")
+    private User user;
 }
