ff3ffe0ea0835833f08a43b0f3b3276e7b4fbb20
Zhenxu Wang <mason123wp@gmail.com>
2023-07-04 15:38:15 -0400

    setup ItemService.java

--- src/main/java/com/mason/picker/service/ItemService.java
+++ src/main/java/com/mason/picker/service/ItemService.java
@@ -1,4 +1,14 @@
 package com.mason.picker.service;
 
+import com.mason.picker.entity.Item;
+
+import java.util.List;
+
 public interface ItemService {
+    Item getItem(Long itemId);
+    Item saveItem(Item item);
+    void deleteItem(Long userId, Long itemId);
+    Item updateItem(Long userId, Long itemId, String newName);
+    List<Item> getItems();
+    List<Item> getUserItems(Long userId);
 }
