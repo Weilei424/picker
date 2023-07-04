148b6294b72604fe39369857f1e57adbb03d610d
Zhenxu Wang <mason123wp@gmail.com>
2023-07-04 15:38:40 -0400

    setup ItemController.java

--- src/main/java/com/mason/picker/web/ItemController.java
+++ src/main/java/com/mason/picker/web/ItemController.java
@@ -1,4 +1,11 @@
 package com.mason.picker.web;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/item")
 public class ItemController {
 }
