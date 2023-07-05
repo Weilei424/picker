1023443566b9b9b6b39b0cbdddcc6a3027f07058
Zhenxu Wang <mason123wp@gmail.com>
2023-07-04 20:16:50 -0400

    implement ItemController.java

--- src/main/java/com/mason/picker/web/ItemController.java
+++ src/main/java/com/mason/picker/web/ItemController.java
@@ -1,11 +1,55 @@
 package com.mason.picker.web;
 
+import com.mason.picker.entity.Item;
+import com.mason.picker.service.ItemService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/item")
 public class ItemController {
+    ItemService itemService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItem(@PathVariable Long id) {
+        return new ResponseEntity<>(itemService.getItem(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Item> saveItem(@RequestBody Item item) {
+        return new ResponseEntity<>(itemService.saveItem(item), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{userId}/{itemId}")
+    public ResponseEntity<HttpStatus> deleteUserItem(@PathVariable Long userId, @PathVariable Long itemId) {
+        itemService.deleteUserItem(userId, itemId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteItem(@PathVariable Long id) {
+        itemService.deleteItem(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/update/{userId}/{itemId}")
+    public ResponseEntity<Item> updateItem(@PathVariable Long userId, @PathVariable Long itemId, @RequestBody Item item) {
+        itemService.updateItem(userId, itemId, item);
+        return new ResponseEntity<>(itemService.getItem(itemId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Item>> getItems() {
+        return new ResponseEntity<>(itemService.getItems(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}/all")
+    public ResponseEntity<List<Item>> getUserItems(@PathVariable Long userId) {
+        return new ResponseEntity<>(itemService.getUserItems(userId), HttpStatus.OK);
+    }
 }
