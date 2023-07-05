4c6a6f3f7e33c991736c14928e11d86e349d69e0
Zhenxu Wang <mason123wp@gmail.com>
2023-07-05 02:17:24 -0400

    implement User.java

--- src/main/java/com/mason/picker/entity/User.java
+++ src/main/java/com/mason/picker/entity/User.java
@@ -1,4 +1,36 @@
 package com.mason.picker.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "username")
+    @NotBlank
+    private String username;
+
+    @JsonIgnore
+    @OneToMany
+    @JoinTable(
+            name = "user_item",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id")
+    )
+    private Set<Item> items;
+
 }
