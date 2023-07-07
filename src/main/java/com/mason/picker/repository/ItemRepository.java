b9569a1968ff821fe8e49c72ba7af1e89407c561
Zhenxu Wang <mason123wp@gmail.com>
2023-07-07 01:52:02 -0400

    add methods for ItemRepository.java

--- src/main/java/com/mason/picker/repository/ItemRepository.java
+++ src/main/java/com/mason/picker/repository/ItemRepository.java
@@ -1,7 +1,15 @@
 package com.mason.picker.repository;
 
 import com.mason.picker.entity.Item;
+import jakarta.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ItemRepository extends CrudRepository<Item, Long> {
+    Optional<Item> findByUserIdAndItemId(Long userId, Long ItemId);
+    @Transactional
+    void deleteByUserIdAndItemId(Long userId, Long ItemId);
+    List<Item> findByUserId(Long userId);
 }
