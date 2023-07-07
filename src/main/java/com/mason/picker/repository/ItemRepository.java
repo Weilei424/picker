9bc09727a04c0ae615cb2817056fc5f3b53f29fa
Zhenxu Wang <mason123wp@gmail.com>
2023-07-07 02:48:41 -0400

    remove unused imports

--- src/main/java/com/mason/picker/repository/ItemRepository.java
+++ src/main/java/com/mason/picker/repository/ItemRepository.java
@@ -1,11 +1,9 @@
 package com.mason.picker.repository;
 
 import com.mason.picker.entity.Item;
-import jakarta.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
     List<Item> findByUserId(Long userId);
