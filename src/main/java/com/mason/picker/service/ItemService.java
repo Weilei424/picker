47838c41a302933bbd38285aee6babdc1b5c878b
Zhenxu Wang <mason123wp@gmail.com>
2023-07-07 02:46:16 -0400

    fix multiple methods

--- src/main/java/com/mason/picker/service/ItemService.java
+++ src/main/java/com/mason/picker/service/ItemService.java
@@ -3,12 +3,13 @@ package com.mason.picker.service;
 import com.mason.picker.entity.Item;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemService {
     Item getItem(Long itemId);
     Item saveItem(Item item);
-    void deleteUserItem(Long userId, Long itemId);
     void deleteItem(Long itemId);
+    void deleteUserItem(Long userId, Long itemId);
     Item updateItem(Long userId, Long itemId, String itemName);
     List<Item> getItems();
     List<Item> getUserItems(Long userId);
--- src/main/java/com/mason/picker/service/ItemServiceImpl.java
+++ src/main/java/com/mason/picker/service/ItemServiceImpl.java
@@ -1,12 +1,14 @@
 package com.mason.picker.service;
 
 import com.mason.picker.entity.Item;
+import com.mason.picker.exception.ItemNotFoundException;
 import com.mason.picker.repository.ItemRepository;
 import com.mason.picker.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -16,35 +18,55 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public Item getItem(Long itemId) {
-        return null;
+        return itemRepository.findById(itemId).get();
     }
 
     @Override
     public Item saveItem(Item item) {
-        return null;
+        return itemRepository.save(item);
+    }
+
+
+
+    @Override
+    public void deleteItem(Long itemId) {
+        itemRepository.deleteById(itemId);
     }
 
     @Override
     public void deleteUserItem(Long userId, Long itemId) {
-
+        List<Item> items = itemRepository.findByUserId(userId);
+        for (Item item : items) {
+            if (item.getId() == itemId)
+                deleteItem(itemId);
+        }
+        throw new ItemNotFoundException(userId);
     }
 
     @Override
-    public void deleteItem(Long itemId) {
-
-    }
-    @Override
-    public Item updateItem(Long userId, Long itemId, Item item) {
-        return null;
+    public Item updateItem(Long userId, Long itemId, String itemName) {
+        List<Item> items = itemRepository.findByUserId(userId);
+        for (Item item : items) {
+            if (item.getId() == itemId) {
+                item.setName(itemName);
+                return itemRepository.save(item);
+            }
+        }
+        throw new ItemNotFoundException(userId);
     }
 
     @Override
     public List<Item> getItems() {
-        return null;
+        return (List<Item>) itemRepository.findAll();
     }
 
     @Override
     public List<Item> getUserItems(Long userId) {
-        return null;
+        return itemRepository.findByUserId(userId);
+    }
+
+    private static Item unwrapItem(Optional<Item> entity, Long userId) {
+        if (entity.isPresent()) return entity.get();
+        else throw new ItemNotFoundException(userId);
     }
 }
