1f010d37655fbb25a54ab2623a9fa1f7423f1b74
Zhenxu Wang <mason123wp@gmail.com>
2023-07-10 02:12:08 -0400

    add api doc annotations

--- src/main/java/com/mason/picker/web/UserController.java
+++ src/main/java/com/mason/picker/web/UserController.java
@@ -2,6 +2,10 @@ package com.mason.picker.web;
 
 import com.mason.picker.entity.User;
 import com.mason.picker.service.UserService;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +19,19 @@ import java.util.List;
 public class UserController {
     UserService userService;
 
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of user")
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
         return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
     }
 
+    @ApiResponse(responseCode = "201", description = "Successful creation of user")
     @PostMapping
     public ResponseEntity<User> saveUser(@RequestBody User user) {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
     }
 
+    @ApiResponse(responseCode = "204", description = "Successful deletion of user")
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
@@ -32,6 +39,7 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @ApiResponse(responseCode = "200", description = "Successful creation of users", content = @Content(array = @ArraySchema(schema = @Schema(implementation = User.class))))
     @GetMapping("/users")
     public  ResponseEntity<List<User>> getUsers() {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
