14154120abfa598d9f4d1bb57a700d40a688d8a2
Zhenxu Wang <mason123wp@gmail.com>
2023-07-17 12:15:31 -0400

    add commandlinerunner

--- src/main/java/com/mason/picker/PickerApplication.java
+++ src/main/java/com/mason/picker/PickerApplication.java
@@ -1,13 +1,23 @@
 package com.mason.picker;
 
+import com.mason.picker.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PickerApplication {
+public class PickerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PickerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User[] users = new User[]{
+				new User("Adam")
+		};
+
+
+	}
 }
