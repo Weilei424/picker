1574453c19000666c57e1784f62cfc8d59d4302d
Zhenxu Wang <mason123wp@gmail.com>
2023-07-24 01:16:12 -0400

    stub still need fix

--- src/main/java/com/mason/picker/PickerApplication.java
+++ src/main/java/com/mason/picker/PickerApplication.java
@@ -1,13 +1,24 @@
 package com.mason.picker;
 
+import com.mason.picker.entity.Item;
 import com.mason.picker.entity.User;
+import com.mason.picker.repository.ItemRepository;
+import com.mason.picker.repository.UserRepository;
+import com.mason.picker.service.ItemService;
+import com.mason.picker.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AllArgsConstructor
 @SpringBootApplication
 public class PickerApplication implements CommandLineRunner {
 
+	UserRepository userRepository;
+	UserService userService;
+	ItemRepository itemRepository;
+	ItemService itemService;
 	public static void main(String[] args) {
 		SpringApplication.run(PickerApplication.class, args);
 	}
@@ -15,9 +26,30 @@ public class PickerApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		User[] users = new User[]{
-				new User("Adam", "123")
+				new User("Adam", "123"),
+				new User("Lee", "321"),
+				new User("Jojo", "456")
 		};
 
+		for (int i = 0; i < users.length; i++) {
+			userRepository.save(users[i]);
+		}
+
+		Item[] items = new Item[]{
+				new Item("Item1"),
+				new Item("Item2"),
+				new Item("Item3"),
+				new Item("Item4"),
+				new Item("Item5"),
+				new Item("Item6"),
+				new Item("Item7"),
+				new Item("Item8"),
+				new Item("Item9")
+		};
+
+		for (int i = 0; i < items.length; i++) {
+			itemRepository.save(items[i]);
+		}
 
 	}
 }
