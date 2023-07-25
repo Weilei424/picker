9b3f426d51496bbfb99beaae8048923541ae9208
Zhenxu Wang <mason123wp@gmail.com>
2023-07-25 19:27:31 -0400

    add openapi annotations

--- src/main/java/com/mason/picker/web/ItemController.java
+++ src/main/java/com/mason/picker/web/ItemController.java
@@ -2,6 +2,7 @@ package com.mason.picker.web;
 
 import com.mason.picker.entity.Item;
 import com.mason.picker.service.ItemService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,21 +16,25 @@ import java.util.List;
 public class ItemController {
     ItemService itemService;
 
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of item")
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItem(@PathVariable Long id) {
         return new ResponseEntity<>(itemService.getItem(id), HttpStatus.OK);
     }
 
+    @ApiResponse(responseCode = "201", description = "Successful creation of item")
     @PostMapping
     public ResponseEntity<Item> saveItem(@RequestBody Item item) {
         return new ResponseEntity<>(itemService.saveItem(item), HttpStatus.CREATED);
     }
 
+    @ApiResponse(responseCode = "201", description = "Successful creation of item under user")
     @PostMapping("/user/{userId}")
     public ResponseEntity<Item> saveUserItem(@PathVariable Long userId, @RequestBody Item item) {
         return new ResponseEntity<>(itemService.saveUserItem(userId, item), HttpStatus.CREATED);
     }
 
+    @ApiResponse(responseCode = "204", description = "Successful deletion of item")
     @DeleteMapping("/{userId}/{itemId}")
     public ResponseEntity<HttpStatus> deleteUserItem(@PathVariable Long userId, @PathVariable Long itemId) {
         itemService.deleteUserItem(userId, itemId);
